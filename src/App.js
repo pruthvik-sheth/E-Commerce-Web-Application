a3fde416ee5fa7cfc5280ca49302bae4b17facb7
DhairyaPatel2210 <pateldhairya283@gmail.com>
2022-06-23 22:31:40 +0530

    Adding server files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import SearchBar from './components/SearchBar'
-import NavButtonBar from './components/NavButtonBar'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import EditProductPage from './pages/EditProductPage'
 import ProductPage from './pages/ProductPage'
 import CartPage from './pages/CartPage'
-import ProductListing from './pages/ProductListingPage'
 import Footer from './components/Footer'
+import ErrorPage from './pages/ErrorPage'
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <SearchBar />
-      <NavButtonBar />
-      <CartPage />
+      <Routes>
+        <Route path='/' exact={true} element={<HomePage />}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/register' element={<RegisterPage />}/>
+        <Route path='/edit' element={<EditProductPage />}/>
+        <Route path='/products' element={<ProductPage />}/>
+        <Route path='/cart' element={<CartPage />}/>
+        <Route path='*' element={<ErrorPage />}/>
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   )
 }
 
 export default App
-
-
-
-
-
