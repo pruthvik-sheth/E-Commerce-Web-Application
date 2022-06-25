c502edf481f3bc0ef052cb610af231c8e3cc0434
DhairyaPatel2210 <pateldhairya283@gmail.com>
2022-06-25 16:56:17 +0530

    authentication without jwt

--- Routes/Authentication/users.js
+++ Routes/Authentication/users.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const route = express.Router()
 const bcrypt = require('bcrypt')
 
@@ -18,14 +17,12 @@ const encodePass = async (pass) => {
 route.post('/register',async (req, res)=> {
     try {
         req.body.password = await encodePass(req.body.password)
-        console.log("User registration before db call" + req.body.password)
         const user = await User.create({
             firstName : req.body.firstname,
             lastName : req.body.lastname,
             email : req.body.email,
             password : req.body.password
         })
-        console.log("User registration after db call")
         res.status(200).json({ message : "User Successfully Registered", success : true})
     } catch (err) {
         res.status(401).json({ message : "User Not Registered" + err, success : false})
@@ -34,13 +31,9 @@ route.post('/register',async (req, res)=> {
 
 route.post('/login', async(req, res) => {
     try {
-        console.log("login route" + req.body.email)
         const user = await User.findOne({ email : req.body.email})
-        console.log(user);
         if (user !== null){
-            console.log("Now checking the pass");
             const passResult = await bcrypt.compare(req.body.password, user.password)
-            console.log("Acd to pass, checking conditions")
             if (passResult) {
                 res.status(200).json({ message : "Logged In Succesfully", success : true})
             }
